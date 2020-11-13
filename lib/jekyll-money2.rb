42c283426c87f716263258e83d36af71efcf4a8b
John Pitchko <john.pitchko@icloud.com>
2020-11-13 07:31:49 -0700

    Fix broken require statements

--- lib/jekyll-money2.rb
+++ lib/jekyll-money2.rb
@@ -1,5 +1,5 @@
-require "jekyll-money/version"
-require 'jekyll-money/core'
-require "jekyll-money/filter"
+require "jekyll-money2/version"
+require 'jekyll-money2/core'
+require "jekyll-money2/filter"
 
 Liquid::Template.register_filter(JekyllMoney2::Filter)
